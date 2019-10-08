33f4039c6b0f799a326d62001358da2540ba8019
li chengpeng <lichengpeng@corp.netease.com>
2019-10-08 20:56:39 +0800

    create skybox project

--- Skybox/main.cpp
+++ Skybox/main.cpp
@@ -1,152 +1,152 @@
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
-#include <exception>
-#include <vector>
-
+#include "engine.hpp"
 #include "shader.hpp"
+#include "camera.hpp"
+#include <vector>
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
 
 namespace gl
 {
-	class RenderPass
+	unsigned int loadCubemap(std::vector<std::string> faces)
 	{
-	public:
-		RenderPass() {}
-		virtual ~RenderPass() {}
+		unsigned int textureID;
+		glGenTextures(1, &textureID);
+		glBindTexture(GL_TEXTURE_CUBE_MAP, textureID);
 
-		virtual void Init() = 0;
-		virtual void Update() = 0;
-
-	private:
-	};
-
-	class DrawTriangle : public RenderPass
-	{
-	public:
-		DrawTriangle() : mVao(0), mVbo(0), mShader() {}
-		virtual ~DrawTriangle() {}
-
-		virtual void Init() override;
-		virtual void Update() override;
-
-	private:
-		GLuint mVao;
-		GLuint mVbo;
-		Shader mShader;
-	};
-
-	void DrawTriangle::Init()
-	{
-		float vertices[] = {
-			-0.5f, -0.5f, 0.0f,
-			 0.5f, -0.5f, 0.0f,
-			 0.0f,  0.5f, 0.0f
-		};
-
-		glGenVertexArrays(1, &mVao);
-		glBindVertexArray(mVao);
+		int width, height, nrChannels;
+		for (unsigned int i = 0; i < faces.size(); i++)
 		{
-			glGenBuffers(1, &mVbo);
-			glBindBuffer(GL_ARRAY_BUFFER, mVbo);
-			glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-			glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-			glEnableVertexAttribArray(0);
-			glBindBuffer(GL_ARRAY_BUFFER, 0);
-		}
-		glBindVertexArray(0);
-
-		mShader.AttachShader(GL_VERTEX_SHADER, "Shader/triangle_vs.glsl");
-		mShader.AttachShader(GL_FRAGMENT_SHADER, "Shader/triangle_fs.glsl");
-		mShader.Link();
-	}
-
-	void DrawTriangle::Update()
-	{
-		mShader.Active();
-		glBindVertexArray(mVao);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
-	}
-
-	class Renderer
-	{
-	public:
-		Renderer();
-		virtual ~Renderer();
-
-		void Init(GLuint width, GLuint height, void (*framebufferSizeCallback)(GLFWwindow*, int, int));
-		void Render();
-
-		void AddPass(RenderPass* pass);
-
-	private:
-		GLFWwindow* mWindow;
-		std::vector<RenderPass*> mRenderPasses;
-	};
-
-	Renderer::Renderer() : mWindow(nullptr)
-	{
-
-	}
-
-	Renderer::~Renderer()
-	{
-
-	}
-
-	void Renderer::Init(GLuint width, GLuint height, void (*framebufferSizeCallback)(GLFWwindow*, int, int))
-	{
-		glfwInit();
-		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
-		glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-#ifdef __APPLE__
-		glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // uncomment this statement to fix compilation on OS X
-#endif
-
-		mWindow = glfwCreateWindow(width, height, "LearnOpenGL", NULL, NULL);
-		if (mWindow == nullptr)
-		{
-			glfwTerminate();
-			throw std::exception("Failed to init GLFW...");
-		}
-
-		glfwMakeContextCurrent(mWindow);
-		glfwSetFramebufferSizeCallback(mWindow, framebufferSizeCallback);
-
-		// glad: load all OpenGL function pointers
-		// ---------------------------------------
-		if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-		{
-			throw std::exception("Failed to init GLAD...");
-		}
-	}
-
-	void Renderer::Render()
-	{
-		for (auto& pass : mRenderPasses)
-		{
-			pass->Init();
-		}
-
-		while (!glfwWindowShouldClose(mWindow))
-		{
-			// processInput(mWindow);
-			for (auto& pass : mRenderPasses)
+			unsigned char *data = stbi_load(faces[i].c_str(), &width, &height, &nrChannels, 0);
+			if (data)
 			{
-				pass->Update();
+				glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+				stbi_image_free(data);
 			}
+			else
+			{
+				std::cout << "Cubemap texture failed to load at path: " << faces[i] << std::endl;
+				stbi_image_free(data);
+			}
+		}
+		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
 
-			glfwSwapBuffers(mWindow);
-			glfwPollEvents();
+		return textureID;
+	}
+
+	class Skybox : public RenderPass
+	{
+	public:
+		Skybox() : mVao(), mCubemapTex(), mCamera(glm::vec3(0.0f, 0.0f, 3.0f))
+		{
+
 		}
 
-		glfwTerminate();
-	}
+		virtual void Init() override
+		{
+			float skyboxVertices[] = {        
+				-1.0f,  1.0f, -1.0f,
+				-1.0f, -1.0f, -1.0f,
+				1.0f, -1.0f, -1.0f,
+				1.0f, -1.0f, -1.0f,
+				1.0f,  1.0f, -1.0f,
+				-1.0f,  1.0f, -1.0f,
 
-	void Renderer::AddPass(RenderPass* pass)
-	{
-		mRenderPasses.emplace_back(pass);
-	}
+				-1.0f, -1.0f,  1.0f,
+				-1.0f, -1.0f, -1.0f,
+				-1.0f,  1.0f, -1.0f,
+				-1.0f,  1.0f, -1.0f,
+				-1.0f,  1.0f,  1.0f,
+				-1.0f, -1.0f,  1.0f,
+
+				1.0f, -1.0f, -1.0f,
+				1.0f, -1.0f,  1.0f,
+				1.0f,  1.0f,  1.0f,
+				1.0f,  1.0f,  1.0f,
+				1.0f,  1.0f, -1.0f,
+				1.0f, -1.0f, -1.0f,
+
+				-1.0f, -1.0f,  1.0f,
+				-1.0f,  1.0f,  1.0f,
+				1.0f,  1.0f,  1.0f,
+				1.0f,  1.0f,  1.0f,
+				1.0f, -1.0f,  1.0f,
+				-1.0f, -1.0f,  1.0f,
+
+				-1.0f,  1.0f, -1.0f,
+				1.0f,  1.0f, -1.0f,
+				1.0f,  1.0f,  1.0f,
+				1.0f,  1.0f,  1.0f,
+				-1.0f,  1.0f,  1.0f,
+				-1.0f,  1.0f, -1.0f,
+
+				-1.0f, -1.0f, -1.0f,
+				-1.0f, -1.0f,  1.0f,
+				1.0f, -1.0f, -1.0f,
+				1.0f, -1.0f, -1.0f,
+				-1.0f, -1.0f,  1.0f,
+				1.0f, -1.0f,  1.0f
+			};
+
+			glGenVertexArrays(1, &mVao);
+			glBindVertexArray(mVao);
+			{
+				GLuint vbo;
+				glGenBuffers(1, &vbo);
+				glBindBuffer(GL_ARRAY_BUFFER, vbo);
+				glBufferData(GL_ARRAY_BUFFER, sizeof(skyboxVertices), &skyboxVertices, GL_STATIC_DRAW);
+				glEnableVertexAttribArray(0);
+				glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+			}
+			glBindVertexArray(0);
+
+			std::vector<std::string> faces
+			{
+				"../Resource/Skybox/right.jpg",
+				"../Resource/Skybox/left.jpg",
+				"../Resource/Skybox/top.jpg",
+				"../Resource/Skybox/bottom.jpg",
+				"../Resource/Skybox/front.jpg",
+				"../Resource/Skybox/back.jpg"
+			};
+			mCubemapTex = loadCubemap(faces);
+
+			mShader.AttachShader(GL_VERTEX_SHADER, "Shader/skybox_vs.glsl");
+			mShader.AttachShader(GL_FRAGMENT_SHADER, "Shader/skybox_fs.glsl");
+			mShader.Link();
+		}
+
+		virtual void Update() override
+		{
+			glm::mat4 model_mat = glm::mat4(1.0f);
+			glm::mat4 projection = glm::perspective(glm::radians(mCamera.Zoom), (float)1280 / (float)720, 0.1f, 100.0f);
+			glm::mat4 view = mCamera.GetViewMatrix();
+
+			mShader.Active();
+			mShader.SetMatrix("model", &model_mat[0][0]);
+			mShader.SetMatrix("view", &view[0][0]);
+			mShader.SetMatrix("projection", &projection[0][0]);
+
+			glBindVertexArray(mVao);
+			glActiveTexture(GL_TEXTURE0);
+			glBindTexture(GL_TEXTURE_CUBE_MAP, mCubemapTex);
+			glDrawArrays(GL_TRIANGLES, 0, 36);
+			glBindVertexArray(0);
+		}
+
+	private:
+		GLuint mCubemapTex;
+		GLuint mVao;
+		Shader mShader;
+		Camera mCamera;
+	};
 }
 
 // glfw: whenever the window size changed (by OS or user resize) this callback function executes
@@ -158,15 +158,15 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 	glViewport(0, 0, width, height);
 }
 
+
 int main()
 {
-	gl::Renderer Renderer;
-	Renderer.Init(1280, 720, framebuffer_size_callback);
+	gl::Engine engine;
+	engine.Init(1280, 720, framebuffer_size_callback);
 
-	gl::DrawTriangle DrawTriangle;
-	Renderer.AddPass(&DrawTriangle);
-
-	Renderer.Render();
+	gl::Skybox skybox;
+	engine.AddPass(&skybox);
+	engine.Render();
 
 	return 0;
 }
