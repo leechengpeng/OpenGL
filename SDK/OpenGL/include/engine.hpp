4a1d3c7418df0ae27dc41f7b06fcfde94699e166
leechengpeng <leechengpeng@126.com>
2019-10-09 21:23:12 +0800

    fiexd drag error

--- SDK/OpenGL/include/engine.hpp
+++ SDK/OpenGL/include/engine.hpp
@@ -74,6 +74,9 @@ namespace gl
 
 		while (!glfwWindowShouldClose(mWindow))
 		{
+			glClearColor(0.05f, 0.05f, 0.05f, 1.0f);
+			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
 			// processInput(mWindow);
 			for (auto& pass : mRenderPasses)
 			{
