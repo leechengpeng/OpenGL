942895af43e3110c60814fb70d567a74cdfb5fe9
li chengpeng <lichengpeng@corp.netease.com>
2019-10-29 13:49:59 +0800

    Update main.cpp

--- Deferred/main.cpp
+++ Deferred/main.cpp
@@ -32,9 +32,7 @@ namespace gl
 				glBindTexture(GL_TEXTURE_2D, gPos);
 				glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB16F, SCR_WIDTH, SCR_HEIGHT, 0, GL_RGB, GL_FLOAT, NULL);
 				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);				
-				//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-				//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);			
 				glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, gPos, 0);
 				// Normal Buffer
 				glGenTextures(1, &gNormal);
