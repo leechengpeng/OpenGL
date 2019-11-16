fb969f48fdd3ae336e80926b9fd761e4e9e12832
li chengpeng <lichengpeng@corp.netease.com>
2019-11-16 16:10:22 +0800

    add project: bloom + hdr

---
+++ Bloom/main.cpp
@@ -0,0 +1,16 @@
+#include "engine.hpp"
+#include "shader.hpp"
+#include "common.hpp"
+#include "common_mesh.hpp"
+#include "controller.hpp"
+
+namespace gl
+{
+	
+}
+
+int main()
+{
+
+	return 0;
+}
---
+++ HDR/main.cpp
@@ -0,0 +1,31 @@
+#include "engine.hpp"
+#include "shader.hpp"
+#include "common.hpp"
+#include "common_mesh.hpp"
+#include "controller.hpp"
+
+namespace gl
+{
+	class HDR : public RenderPass
+	{
+	public:
+		virtual void Init() override
+		{
+
+		}
+
+		virtual void Update(const STime& time) override
+		{
+
+		}
+
+	private:
+
+	};
+}
+
+int main()
+{
+
+	return 0;
+}
