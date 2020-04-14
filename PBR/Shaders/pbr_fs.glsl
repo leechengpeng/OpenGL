b0426d83a1f967b899d94168e856ca23a222564d
li chengpeng <lichengpeng@corp.netease.com>
2020-04-14 19:09:43 +0800

    重构shader

--- PBR/Shaders/pbr_fs.glsl
+++ PBR/Shaders/pbr_fs.glsl
@@ -65,6 +65,46 @@ vec3 fresnelSchlick(float cosTheta, vec3 F0)
     return F0 + (1.0 - F0) * pow(1.0 - cosTheta, 5.0);
 }
 
+//
+vec3 brdf_diffuse(vec3 albedo)
+{
+    return albedo / PI;
+}
+
+// Bidirectional Reflective Distribution Function
+vec3 brdf(vec3 Wi, vec3 Wo, vec3 N, vec3 albedo, float roughness, float metallic)
+{
+    vec3 F0 = vec3(0.04);
+    F0 = mix(F0, albedo, metallic);
+
+    vec3 H = normalize(Wi + Wo);
+
+    // diffuse
+    vec3  diffuse = albedo / PI;
+
+    // specular
+    // Cook-Torrance BRDF
+    float NDF = DistributionGGX(N, H, roughness);   
+    float G   = GeometrySmith(N, Wo, Wi, roughness);      
+    vec3  F   = fresnelSchlick(clamp(dot(H, Wo), 0.0, 1.0), F0);
+    vec3  nominator   = NDF * G * F; 
+    float denominator = 4 * max(dot(N, Wo), 0.0) * max(dot(N, Wi), 0.0);
+    vec3  specular    = nominator / max(denominator, 0.001); // prevent divide by zero for NdotV=0.0 or NdotL=0.0
+        
+    // kS is equal to Fresnel
+    vec3 kS = F;
+    // for energy conservation, the diffuse and specular light can't
+    // be above 1.0 (unless the surface emits light); to preserve this
+    // relationship the diffuse component (kD) should equal 1.0 - kS.
+    vec3 kD = vec3(1.0) - kS;
+    // multiply kD by the inverse metalness such that only non-metals 
+    // have diffuse lighting, or a linear blend if partly metal (pure metals
+    // have no diffuse light).
+    kD *= 1.0 - metallic;
+
+    return kD * diffuse + specular; // note that we already multiplied the BRDF by the Fresnel (kS) so we won't multiply by kS again
+}
+
 void main()
 {
     vec3  albedo    = vec3(1.0);
@@ -88,9 +128,6 @@ void main()
     vec3 N = normalize(Normal);
     vec3 V = normalize(uCamPos - WorldPos);
 
-    vec3 F0 = vec3(0.04);
-    F0 = mix(F0, albedo, metallic);
-
     vec3 Lo =- vec3(0.0);
     for (int i = 0; i < 4; ++i)
     {
@@ -102,31 +139,8 @@ void main()
         float attenuation = 1.0 / (distance * distance);
         vec3 radiance = uLightColor[i] * attenuation;
 
-        // Cook-Torrance BRDF
-        float NDF = DistributionGGX(N, H, roughness);   
-        float G   = GeometrySmith(N, V, L, roughness);      
-        vec3  F   = fresnelSchlick(clamp(dot(H, V), 0.0, 1.0), F0);
-           
-        vec3  nominator   = NDF * G * F; 
-        float denominator = 4 * max(dot(N, V), 0.0) * max(dot(N, L), 0.0);
-        vec3  specular    = nominator / max(denominator, 0.001); // prevent divide by zero for NdotV=0.0 or NdotL=0.0
-        
-        // kS is equal to Fresnel
-        vec3 kS = F;
-        // for energy conservation, the diffuse and specular light can't
-        // be above 1.0 (unless the surface emits light); to preserve this
-        // relationship the diffuse component (kD) should equal 1.0 - kS.
-        vec3 kD = vec3(1.0) - kS;
-        // multiply kD by the inverse metalness such that only non-metals 
-        // have diffuse lighting, or a linear blend if partly metal (pure metals
-        // have no diffuse light).
-        kD *= 1.0 - metallic;
-
-        // scale light by NdotL
-        float NdotL = max(dot(N, L), 0.0);        
-
-        // add to outgoing radiance Lo
-        Lo += (kD * albedo / PI + specular) * radiance * NdotL;  // note that we already multiplied the BRDF by the Fresnel (kS) so we won't multiply by kS again
+        vec3 fr = brdf(L, V, N, albedo, roughness, metallic);
+        Lo += fr * radiance * max(dot(N, L), 0.0);
     }
     
     // ambient lighting (note that the next IBL tutorial will replace 
@@ -139,7 +153,7 @@ void main()
     // HDR tonemapping
     color = color / (color + vec3(1.0));
     // gamma correct
-    color = pow(color, vec3(1.0/2.2)); 
+    // color = pow(color, vec3(1.0/2.2)); 
 
     FragColor = vec4(color, 1.0);
 }
